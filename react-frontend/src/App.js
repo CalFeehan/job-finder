68e809c5ed5d79868b93cb6ab7cd355547f5e14f
Callum Feehan <72271107+CalFeehan@users.noreply.github.com>
2021-08-20 20:41:46 +0100

    Created react app

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,18 @@
-
+import {Grid} from "@material-ui/core"
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Job Finder</h1>
-    </div>
+    <>
+      <Grid container style={{height: "100%"}}>
+        <Grid item style={{width: "3.5rem", background: "red"}}>
+
+        </Grid>
+        <Grid item style={{flexGrow: 1, background: "blue"}}>
+          
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
