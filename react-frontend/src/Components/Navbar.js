ede8c899d8a36038da3f90f6a06f3158c5d24c93
Callum Feehan <72271107+CalFeehan@users.noreply.github.com>
2021-08-24 16:42:02 +0100

    fixed hover text

--- react-frontend/src/Components/Navbar.js
+++ react-frontend/src/Components/Navbar.js
@@ -1,49 +1,41 @@
 import { Button, Grid } from "@material-ui/core"
 import { useState } from 'react'
 
+
 export default function Navbar(props) {
     const [isDashboardHover, setDashboardHover] = useState(false);
     const [isProfileHover, setProfileHover] = useState(false);
     const [isContactHover, setContactHover] = useState(false);
     const [isLogoutHover, setLogoutHover] = useState(false);
 
+    const buttonStyle = {width: "3.5rem", height: "3.5rem", minWidth: "unset", backgroundRepeat: "no-repeat", backgroundSize: "50%", backgroundPosition: "center", margin: "1rem 0rem 1rem 0rem"};
+    const hoverStyle = {position: "absolute", color: "#2F4067", fontWeight: "bold"};
+
     return (
             <Grid container direction="column" style={{width: "3.5rem", background: "#2F4067"}} {...props}>
-                <Grid style={{width: "3.5rem", height: "3.5rem", minWidth: "unset", background: "url('/assets/Logo.png')", backgroundRepeat: "no-repeat", backgroundSize: "50%", backgroundPosition: "center", margin: "1rem 0rem 1rem 0rem"}}/>
+
+                <Grid style={Object.assign({background: "url('/assets/Logo.png')"}, buttonStyle)}/>
+
                 <Grid item container justifyContent= "center" direction="column" style={{flexGrow: 1}}>
-                    <Button
-                    onMouseEnter={() => setDashboardHover(true)}
-                    onMouseLeave={() => setDashboardHover(false)}
-                    style={{width: "3.5rem", height: "3.5rem", minWidth: "unset", background: "url('/assets/Icon-dashboard.png')", backgroundRepeat: "no-repeat", backgroundSize: "50%", backgroundPosition: "center", margin: "1rem 0rem 1rem 0rem"}}/>
-                    {isDashboardHover && (
-                        <div style={{position: "absolute", color: "#2F4067", margin: "0rem 0rem 10.5rem 4rem", fontWeight: "bold"}}>
-                            Dashboard
-                        </div> )}
-                    <Button 
-                    onMouseEnter={() => setProfileHover(true)}
-                    onMouseLeave={() => setProfileHover(false)}
-                    style={{width: "3.5rem", height: "3.5rem", minWidth: "unset", background: "url('/assets/Icon-profile.png')", backgroundRepeat: "no-repeat", backgroundSize: "50%", backgroundPosition: "center", margin: "1rem 0rem 1rem 0rem"}}/>
-                    {isProfileHover && (
-                        <div style={{position: "absolute", color: "#2F4067", margin: "0rem 0rem 0rem 4rem", fontWeight: "bold"}}>
-                            Profile
-                        </div> )}
-                    <Button
-                    onMouseEnter={() => setContactHover(true)}
-                    onMouseLeave={() => setContactHover(false)}
-                    style={{width: "3.5rem", height: "3.5rem", minWidth: "unset", background: "url('/assets/Icon-contact.png')", backgroundRepeat: "no-repeat", backgroundSize: "50%", backgroundPosition: "center", margin: "1rem 0rem 1rem 0rem"}}/>
-                    {isContactHover && (
-                        <div style={{position: "absolute", color: "#2F4067", margin: "10.5rem 0rem 0rem 4rem", fontWeight: "bold"}}>
-                            Contact
-                        </div> )}
+
+                    <Button style={Object.assign({background: "url('/assets/Icon-dashboard.png')"}, buttonStyle)}
+                    onMouseEnter={() => setDashboardHover(true)} onMouseLeave={() => setDashboardHover(false)}/>
+                    {isDashboardHover && (<div style={Object.assign({margin: "-5.25rem 0rem 0rem 4rem"}, hoverStyle)}> Dashboard </div>)}
+
+                    <Button style={Object.assign({background: "url('/assets/Icon-profile.png')"}, buttonStyle)}
+                    onMouseEnter={() => setProfileHover(true)} onMouseLeave={() => setProfileHover(false)}/>
+                    {isProfileHover && (<div style={Object.assign({margin: "0rem 0rem 0rem 4rem"}, hoverStyle)}> Profile </div>)}
+
+                    <Button style={Object.assign({background: "url('/assets/Icon-contact.png')"}, buttonStyle)}
+                    onMouseEnter={() => setContactHover(true)} onMouseLeave={() => setContactHover(false)}/>
+                    {isContactHover && (<div style={Object.assign({margin: "5.25rem 0rem 0rem 4rem"}, hoverStyle)}> Contact </div>)}
+
                 </Grid>
-                <Button 
-                onMouseEnter={() => setLogoutHover(true)}
-                onMouseLeave={() => setLogoutHover(false)}
-                style={{width: "3.5rem", height: "3.5rem", minWidth: "unset", background: "url('/assets/Icon-logout.png')", backgroundRepeat: "no-repeat", backgroundSize: "50%", backgroundPosition: "center", margin: "1rem 0rem 1rem 0rem"}}/>
-                {isLogoutHover && (
-                        <div style={{position: "absolute", color: "#2F4067", bottom: "0px", margin: "0rem 0rem 2rem 4rem", fontWeight: "bold"}}>
-                            Logout
-                        </div> )}
+
+                <Button style={Object.assign({background: "url('/assets/Icon-logout.png')"}, buttonStyle)}
+                onMouseEnter={() => setLogoutHover(true)} onMouseLeave={() => setLogoutHover(false)}/>
+                {isLogoutHover && (<div style={Object.assign({bottom: "0px", margin: "0rem 0rem 2rem 4rem"}, hoverStyle)}> Logout </div>)}
+
             </Grid>
     );
 }
